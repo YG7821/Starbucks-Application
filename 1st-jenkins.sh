ab002bc3c6be6c1ebe61bce32a9b8a905ab0d1cc
YG7821 <yogyata_yogita@yahoo.co.in>
2026-09-04 11:06:33 +0530

    Enhance Jenkins installation script
    Updated installation script for Jenkins to include JRE and JDK installation commands, and added version checks.

--- 1st-jenkins.sh
+++ 1st-jenkins.sh
@@ -1,16 +1,27 @@
-# https://www.jenkins.io/doc/book/installing/linux/ 
+# https://www.jenkins.io/doc/book/installing/linux/
 
 
-openjdk 21.0.8 2025-07-15
-OpenJDK Runtime Environment (build 21.0.8+9-Debian-1)
-OpenJDK 64-Bit Server VM (build 21.0.8+9-Debian-1, mixed mode, sharing)
+sudo apt update
+FOR JRE                    sudo apt install fontconfig openjdk-21-jre -y
+FOR JDK                    sudo apt install openjdk-21-jdk
+
+java -version
+
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-sudo apt install jenkins
 
+sudo apt update
+
+sudo apt install jenkins -y
+
+jenkins --version
+
+systemctl status jenkins
+systemctl stop jenkins
+systemctl start jenkins
 
